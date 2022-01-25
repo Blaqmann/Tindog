532b04fde266a18def2fcc0c48a623a371a56649
Blaqmann <seyiakintoye7@gmail.com>
2022-01-25 16:55:41 +0100

    Tried sending Tindog's files initially

---
+++ index.js
@@ -0,0 +1,23 @@
+// //
+// // var x= document.querySelectorAll("a")[0].style.color= "black";
+// // x.slideUp().slideDown();
+// //
+//
+//
+// $("a").slideDown();
+//
+// $("a").fadeOut();
+//
+// $("a").on("click", function() {
+//   $("a").slideDown();
+// }
+// );
+//
+//
+//
+//
+//
+// // $("h1").on("mouseover", function() {
+// //     $("h1").css("color", "red");
+// // }
+// // );
